9cd47fd89fbc9253f94d7ec1f67cb4349b0198e1
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-01-15 02:05:03 +0530

    added get cat by id controller and route

--- controllers/catControllers/cat.js
+++ controllers/catControllers/cat.js
@@ -1,58 +1,73 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 export const uploadCatController = async (req, res) => {
-    
-
-
-    try{
-        const {name,breed,location,imageUrl,description} = req.body;
-        if(!name || !breed || !location || !imageUrl || !description){
-        return res.status(400).json({
-            success: false,
-            message: "All fields are required"
-        });
+  try {
+    const { name, breed, location, imageUrl, description } = req.body;
+    if (!name || !breed || !location || !imageUrl || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
 
-        const cat = await prisma.cat.create({
-            data: {
-                name,
-                breed,
-                location,
-                imageUrl,
-                description
-            }
-        })
-        return res.status(201).json({
-            success: true,
-            message: "Cat created successfully",
-            data: cat
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error"
-        })
-    }
-    
-    
-}
-
-
+    const cat = await prisma.cat.create({
+      data: {
+        name,
+        breed,
+        location,
+        imageUrl,
+        description,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Cat created successfully",
+      cat,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
 export const getAllCatsController = async (req, res) => {
-    try{
-        const data = await prisma.cat.findMany();
-        return res.status(200).json({
-            success: true,
-            message: "Cats fetched successfully",
-            data
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Failed to fetch"
-        })
-    }
-}
+  try {
+    const cats = await prisma.cat.findMany();
+    return res.status(200).json({
+      success: true,
+      message: "Cats fetched successfully",
+      cats,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server failed to fetch cat",
+    });
+  }
+};
+
+export const getCatByIdController = async (req, res) => {
+  const catId = req.params.catId;
+  try {
+    const cat = await prisma.cat.findUnique({
+      where: {
+        id: Number(catId),
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Cat fetched successfully",
+      cat,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server failed to fetch cat",
+    });
+  }
+};
--- routes/catRouters/cat.route.js
+++ routes/catRouters/cat.route.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import { getAllCatsController, uploadCatController } from '../index.js';
-
+import express from "express";
+import {
+  getAllCatsController,
+  getCatByIdController,
+  uploadCatController,
+} from "../index.js";
 
 const router = express.Router();
 
+router.get("/get-cats", getAllCatsController);
+router.post("/add-cat", uploadCatController);
+router.get("/get-cat/:catId", getCatByIdController);
 
-router.get('/get-cat',getAllCatsController)
-router.post('/add-cat',uploadCatController)
-
-
-export default router
+export default router;
