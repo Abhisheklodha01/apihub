2223765a4c3058766d10083d0a13c5a6ff575285
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-03-19 03:41:48 +0000

    modified cors policy

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
-app.options(corsOptions)
+app.options( "*", corsOptions)
 
 connectWithMongoDB();
 
